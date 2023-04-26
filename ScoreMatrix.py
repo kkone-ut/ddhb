521057a95e3a91d205977a7e505059f76da6c775
yutake2000 <yutake2000@gmail.com>
2023-04-26 11:38:57 +0900

    相対確率が発散しないように変更

--- ScoreMatrix.py
+++ ScoreMatrix.py
@@ -10,16 +10,10 @@ class ScoreMatrix:
         self.N = game_setting.player_num
         self.M = len(game_setting.role_num_map)
         # score_matrix[エージェント1, 役職1, エージェント2, 役職2]: エージェント1が役職1、エージェント2が役職2である相対確率の対数
+        # -infで相対確率は0になる
         self.score_matrix: np.ndarray = np.zeros((self.N, self.M, self.N, self.M))
         self.player = _player
         self.me = _player.me
-
-    def update(self, game_info: GameInfo, game_setting: GameSetting) -> None:
-
-        N = self.N
-        M = self.M
-
-        pass
 
     # 公開情報から推測する
 
@@ -37,10 +31,15 @@ class ScoreMatrix:
         id = target.agent_idx()
 
         if species == Species.WEREWOLF:
-            self.score_matrix[id, Role.WEREWOLF, id, Role.WEREWOLF] = +float('inf')
+            # 人狼であることが確定しているので、他の役職のスコアを-inf(相対確率0)にする
+            # WEREWOLFのスコアを+infにするだけでも良いが、相対確率が無限に発散するので嬉しくない
+            self.score_matrix[id, :, id, :] = -float('inf')
+            self.score_matrix[id, Role.WEREWOLF, id, Role.WEREWOLF] = 0
+
             # こう書くことも可能だが、自分が占い師であることは確定しているので、上の書き方に統一する
             # self.score_matrix[self.me.agent_idx(), Role.SEER, id, Role.WEREWOLF] = +float('inf')
         elif species == Species.HUMAN:
+            # 人狼でないことが確定しているので、人狼のスコアを-inf(相対確率0)にする
             self.score_matrix[id, Role.WEREWOLF, id, Role.WEREWOLF] = -float('inf')
         else:
             pass # 万が一不確定(Species.UNC, Species.ANY)の場合は何もしない
@@ -48,12 +47,14 @@ class ScoreMatrix:
     def my_identified(self, game_info: GameInfo, game_setting: GameSetting, target: Agent, species: Species) -> None:
         id = target.agent_idx()
 
+        # my_divinedと同様
         if species == Species.WEREWOLF:
-            self.score_matrix[id, Role.WEREWOLF, id, Role.WEREWOLF] = +float('inf')
+            self.score_matrix[id, :, id, :] = -float('inf')
+            self.score_matrix[id, Role.WEREWOLF, id, Role.WEREWOLF] = 0
         elif species == Species.HUMAN:
             self.score_matrix[id, Role.WEREWOLF, id, Role.WEREWOLF] = -float('inf')
         else:
-            pass # 万が一不確定(Species.UNC, Species.ANY)の場合は何もしない
+            pass
 
     def my_guarded(self, game_info: GameInfo, game_setting: GameSetting, target: Agent) -> None:
         id = target.agent_idx()
