2487f1dcf5b05a9bc85dc63dd55f7bcfd9aa86f3
yutake2000 <yutake2000@gmail.com>
2023-06-06 18:42:40 +0900

    role の指定を実装

--- client.sh
+++ client.sh
@@ -1,1 +1,19 @@
-python3 start.py -p 10000 -h 127.0.0.1
+port="10000"
+host="127.0.0.1"
+role="none" # 指定するときはすべて大文字
+
+while getopts ":p:h:r:" opt; do
+  case $opt in
+    p) port="$OPTARG"
+    ;;
+    h) host="$OPTARG"
+    ;;
+    r) role="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+role=`echo $role | tr "[:lower:]" "[:upper:]"`
+python3 start.py -p $port -h $host -r $role
