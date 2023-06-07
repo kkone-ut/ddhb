ab1d1b27c039598825d3b2dddc6e98e99651113b
yutake2000 <yutake2000@gmail.com>
2023-06-07 20:58:35 +0900

    -a all を追加

--- server.sh
+++ server.sh
@@ -59,33 +59,62 @@ echo "agent=${n}" >> AutoStarter.ini
 
 # クライアントを接続しない場合は全プレイヤーを記述
 # クライアントを接続する場合は最後のプレイヤーを除いて記述
-for i in $(seq 1 $n)
-do
-    if [ "$i" -ne "$n" ] || ! "$client"; then
-        if [ "$otherAgents" = "basket" ]; then
+
+# otherAgents が "all" でないなら
+if [ "$otherAgents" = "all" ]; then
+    if [ "$n" = 5 ]; then
+        echo "BasketPlayer,java,org.aiwolf.Basket.BasketRoleAssignPlayer" >> AutoStarter.ini
+        echo "WasabiPlayer,java,jp.ac.shibaura_it.ma15082.player.WasabiRoleAssignPlayer" >> AutoStarter.ini
+        echo "TomatoPlayer,java,com.gmail.toooo1718tyan.Player.RoleAssignPlayer" >> AutoStarter.ini
+        echo "IOHPlayer,java,org.aiwolf.IOH.IOHRoleAssignPlayer" >> AutoStarter.ini
+        echo "KarmaPlayer,java,aiwolf.org.karma.KarmaRoleAssignPlayer" >> AutoStarter.ini
+    elif [ "$n" = 15 ]; then
+        for i in $(seq 1 2)
+        do
             echo "BasketPlayer${i},java,org.aiwolf.Basket.BasketRoleAssignPlayer" >> AutoStarter.ini
-        elif [ "$otherAgents" = "wasabi" ]; then
             echo "WasabiPlayer${i},java,jp.ac.shibaura_it.ma15082.player.WasabiRoleAssignPlayer" >> AutoStarter.ini
-        elif [ "$otherAgents" = "tomato" ]; then
             echo "TomatoPlayer${i},java,com.gmail.toooo1718tyan.Player.RoleAssignPlayer" >> AutoStarter.ini
-        elif [ "$otherAgents" = "ioh" ]; then
             echo "IOHPlayer${i},java,org.aiwolf.IOH.IOHRoleAssignPlayer" >> AutoStarter.ini
-        elif [ "$otherAgents" = "karma" ]; then
             echo "KarmaPlayer${i},java,aiwolf.org.karma.KarmaRoleAssignPlayer" >> AutoStarter.ini
-        elif [ "$otherAgents" = "toku" ]; then
             echo "TOKUPlayer${i},java,org.aiwolf.TOKU.TOKURoleAssginPlayer" >> AutoStarter.ini # TOKURoleAssginPlayer (スペルミス)
-        elif [ "$otherAgents" = "camellia" ]; then
             echo "CamelliaPlayer${i},java,camellia.aiwolf.demo.DemoRoleAssignPlayer" >> AutoStarter.ini
-        elif [ "$otherAgents" = "daisyo" ]; then
+        done
+        if [ "$client" = "false" ]; then
             echo "DaisyoPlayer${i},java,org.aiwolf.daisyo.RoleAssignPlayer" >> AutoStarter.ini
-        elif [ "$otherAgents" = "ddhb" ]; then
-            echo "PythonPlayer${i},python,../start.py" >> AutoStarter.ini
-        else
-            echo Agent \"$otherAgents\" is not supported.
-            exit 1
         fi
+    else
+        echo "Error: number of players is not 5 or 15"
+        exit 1
     fi
-done
+else
+    for i in $(seq 1 $n)
+    do
+        if [ "$i" -ne "$n" ] || ! "$client"; then
+            if [ "$otherAgents" = "basket" ]; then
+                echo "BasketPlayer${i},java,org.aiwolf.Basket.BasketRoleAssignPlayer" >> AutoStarter.ini
+            elif [ "$otherAgents" = "wasabi" ]; then
+                echo "WasabiPlayer${i},java,jp.ac.shibaura_it.ma15082.player.WasabiRoleAssignPlayer" >> AutoStarter.ini
+            elif [ "$otherAgents" = "tomato" ]; then
+                echo "TomatoPlayer${i},java,com.gmail.toooo1718tyan.Player.RoleAssignPlayer" >> AutoStarter.ini
+            elif [ "$otherAgents" = "ioh" ]; then
+                echo "IOHPlayer${i},java,org.aiwolf.IOH.IOHRoleAssignPlayer" >> AutoStarter.ini
+            elif [ "$otherAgents" = "karma" ]; then
+                echo "KarmaPlayer${i},java,aiwolf.org.karma.KarmaRoleAssignPlayer" >> AutoStarter.ini
+            elif [ "$otherAgents" = "toku" ]; then
+                echo "TOKUPlayer${i},java,org.aiwolf.TOKU.TOKURoleAssginPlayer" >> AutoStarter.ini # TOKURoleAssginPlayer (スペルミス)
+            elif [ "$otherAgents" = "camellia" ]; then
+                echo "CamelliaPlayer${i},java,camellia.aiwolf.demo.DemoRoleAssignPlayer" >> AutoStarter.ini
+            elif [ "$otherAgents" = "daisyo" ]; then
+                echo "DaisyoPlayer${i},java,org.aiwolf.daisyo.RoleAssignPlayer" >> AutoStarter.ini
+            elif [ "$otherAgents" = "ddhb" ]; then
+                echo "PythonPlayer${i},python,../start.py" >> AutoStarter.ini
+            else
+                echo Agent \"$otherAgents\" is not supported.
+                exit 1
+            fi
+        fi
+    done
+fi
 
 if "$loop"; then
     while true
