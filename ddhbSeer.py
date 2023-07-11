22dd8d7d63afe5cd950a234444f80ac53536d916
Kazuya Kadokura <onepiece9631k@g.ecc.u-tokyo.ac.jp>
2023-07-11 19:59:18 +0900

    村人・占い・人狼の5人村の戦略追加

--- ddhbSeer.py
+++ ddhbSeer.py
@@ -20,8 +20,9 @@ from typing import Deque, List, Optional
 
 from aiwolf import (Agent, ComingoutContentBuilder, Content,
                     DivinedResultContentBuilder, GameInfo, GameSetting, Judge,
-                    Role, Species, VoteContentBuilder)
-from aiwolf.constant import AGENT_NONE
+                    Role, Species, VoteContentBuilder,
+                    RequestContentBuilder)
+from aiwolf.constant import AGENT_NONE, AGENT_ANY
 
 from const import CONTENT_SKIP
 from ddhbVillager import ddhbVillager
@@ -46,6 +47,9 @@ class ddhbSeer(ddhbVillager):
     
     strategies: List[bool] # 戦略フラグのリスト
     others_seer_co: List[Agent] # 他の占い師のCOリスト
+    
+    new_target: Agent # 偽の占い対象
+    new_result: Species # 偽の占い結果
 
 
     def __init__(self) -> None:
@@ -75,6 +79,9 @@ class ddhbSeer(ddhbVillager):
         
         self.others_seer_co.clear()
         
+        self.new_target = AGENT_NONE
+        self.new_result = Species.WEREWOLF
+        
         # 戦略A: 初日CO
         if self.strategyA:
             self.co_date = 1
@@ -87,6 +94,8 @@ class ddhbSeer(ddhbVillager):
     # 昼スタート→OK
     def day_start(self) -> None:
         super().day_start()
+        self.new_target = AGENT_NONE
+        self.new_result = Species.WEREWOLF
         # Process a divination result.
         # 占い結果
         judge: Optional[Judge] = self.game_info.divine_result
@@ -103,55 +112,104 @@ class ddhbSeer(ddhbVillager):
 
     # CO、結果報告、投票宣言→OK
     def talk(self) -> Content:
-        # ---------- CO ----------
-        # 絶対にCOする→1,2,3
-        # 1: 予定の日にち
-        if not self.has_co and self.game_info.day == self.co_date:
-            self.has_co = True
-            return Content(ComingoutContentBuilder(self.me, Role.SEER))
-        # 2: 人狼発見
-        if not self.has_co and self.werewolves:
-            self.has_co = True
-            return Content(ComingoutContentBuilder(self.me, Role.SEER))
-        # 3: 他の占い師がCOしたら(CCO)
-        # 注意：comingout_mapには、自分は含まれていない
-        self.others_seer_co = [a for a in self.comingout_map if self.comingout_map[a] == Role.SEER]
-        if not self.has_co and self.others_seer_co:
-            self.has_co = True
-            return Content(ComingoutContentBuilder(self.me, Role.SEER))
         
-        # ---------- 結果報告 ----------
-        # Report the divination result after doing comingout.
-        if self.has_co and self.my_judge_queue:
-            judge: Judge = self.my_judge_queue.popleft()
-            # ----- 5人村 -----
-            if self.N == 5:
-                # 黒結果
-                if judge.result == Species.WEREWOLF:
-                    return Content(DivinedResultContentBuilder(judge.target, judge.result))
-                # 白結果 → 状況に応じて、結果を捏造して報告する
-                elif judge.result == Species.HUMAN:
-                    others_co_num: int = len(self.others_seer_co)
-                    new_target: Agent = AGENT_NONE
-                    if others_co_num == 0:
-                        new_target = self.role_predictor.chooseMostLikely(Role.WEREWOLF, self.not_divined_agents)
-                    elif others_co_num >= 1:
-                        new_target = self.role_predictor.chooseMostLikely(Role.WEREWOLF, self.others_seer_co)
-                    if new_target != AGENT_NONE:
-                        new_target = judge.target
-                    return Content(DivinedResultContentBuilder(new_target, judge.result))
-            # ----- 15人村 -----
-            elif self.N == 15:
+        day: int = self.game_info.day
+        turn: int = self.talk_turn
+        self.others_seer_co = [a for a in self.comingout_map if self.comingout_map[a] == Role.SEER]
+        others_co_num: int = len(self.others_seer_co)
+        
+        # ---------- 5人村 ----------
+        if self.N == 5:
+            if day == 1:
+                # ----- CO -----
+                if turn == 1:
+                    if not self.has_co:
+                        self.has_co = True
+                        return Content(ComingoutContentBuilder(self.me, Role.SEER))
+                # ----- 結果報告 -----
+                elif turn == 2:
+                    if self.has_co and self.my_judge_queue:
+                        judge: Judge = self.my_judge_queue.popleft()
+                        self.new_target = judge.target
+                        self.new_result = judge.result
+                        # 黒結果→そのまま報告
+                        if judge.result == Species.WEREWOLF:
+                            return Content(DivinedResultContentBuilder(judge.target, judge.result))
+                        # 白結果→状況に応じて報告
+                        elif judge.result == Species.HUMAN:
+                            self.new_result = Species.WEREWOLF
+                            if others_co_num == 0:
+                                self.new_target = self.role_predictor.chooseMostLikely(Role.WEREWOLF, self.not_divined_agents)
+                            else:
+                                self.new_target = self.role_predictor.chooseMostLikely(Role.WEREWOLF, self.others_seer_co)
+                            if self.new_target == AGENT_NONE:
+                                self.new_target = judge.target
+                                self.new_result = judge.result
+                            return Content(DivinedResultContentBuilder(self.new_target, self.new_result))
+                # ----- VOTE and REQUEST -----
+                elif turn == 3 or turn == 5:
+                    return Content(VoteContentBuilder(self.new_target))
+                elif turn == 4 or turn == 6:
+                    return Content(RequestContentBuilder(AGENT_ANY, Content(VoteContentBuilder(self.new_target))))
+                else:
+                    return CONTENT_SKIP
+            elif day >= 2:
+                # ----- 結果報告 -----
+                if turn == 1:
+                    # 正しい結果でOK
+                    if self.has_co and self.my_judge_queue:
+                        judge: Judge = self.my_judge_queue.popleft()
+                        self.new_target = judge.target
+                        self.new_result = judge.result
+                        if judge.result == Species.WEREWOLF:
+                            return Content(DivinedResultContentBuilder(judge.target, judge.result))
+                        # 白結果なら、残りの1人に黒結果
+                        elif judge.result == Species.HUMAN:
+                            self.new_targett = self.random_select(self.get_alive_others(self.not_divined_agents))
+                            self.new_result = Species.WEREWOLF
+                            return Content(DivinedResultContentBuilder(self.new_target, self.new_result))
+                # ----- VOTE and REQUEST -----
+                elif turn == 2 or turn == 4:
+                    return Content(VoteContentBuilder(self.new_target))
+                elif turn == 3 or turn == 5:
+                    return Content(RequestContentBuilder(AGENT_ANY, Content(VoteContentBuilder(self.new_target))))
+                else:
+                    return CONTENT_SKIP
+            else:
+                return CONTENT_SKIP
+        
+        # ---------- 15人村 ----------
+        elif self.N == 15:
+            # ---------- CO ----------
+            # 絶対にCOする→1,2,3
+            # 1: 予定の日にち
+            if not self.has_co and self.game_info.day == self.co_date:
+                self.has_co = True
+                return Content(ComingoutContentBuilder(self.me, Role.SEER))
+            # 2: 人狼発見
+            if not self.has_co and self.werewolves:
+                self.has_co = True
+                return Content(ComingoutContentBuilder(self.me, Role.SEER))
+            # 3: 他の占い師がCOしたら(CCO)
+            # 注意：comingout_mapには、自分は含まれていない
+            self.others_seer_co = [a for a in self.comingout_map if self.comingout_map[a] == Role.SEER]
+            if not self.has_co and self.others_seer_co:
+                self.has_co = True
+                return Content(ComingoutContentBuilder(self.me, Role.SEER)) 
+            # ---------- 結果報告 ----------
+            # Report the divination result after doing comingout.
+            if self.has_co and self.my_judge_queue:
+                judge: Judge = self.my_judge_queue.popleft()
                 # 正しい結果を報告する
                 return Content(DivinedResultContentBuilder(judge.target, judge.result))
-        
-        # ---------- 投票宣言 ----------
-        # self.vote()の利用
-        if self.talk_turn >= 2 and self.vote_candidate == AGENT_NONE:
-            self.vote_candidate = self.vote()
-            return Content(VoteContentBuilder(self.vote_candidate))
-        
-        return CONTENT_SKIP
+            # ---------- 投票宣言 ----------
+            # self.vote()の利用
+            if self.talk_turn >= 2 and self.vote_candidate == AGENT_NONE:
+                self.vote_candidate = self.vote()
+                return Content(VoteContentBuilder(self.vote_candidate))
+            return CONTENT_SKIP
+        else:
+            return CONTENT_SKIP
 
 
     # 投票対象→OK
