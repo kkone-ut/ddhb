48a35ee5d16a81c5e7b19944ac3fbe99bdc4e385
Kazuya Kadokura <onepiece9631k@g.ecc.u-tokyo.ac.jp>
2023-11-08 20:04:02 +0900

    実行方法の追加。コメント追加

--- client.sh
+++ client.sh
@@ -1,39 +1,39 @@
-port="10000"
-host="127.0.0.1"
-role="none" # 指定するときはすべて大文字
-times=1
+port="10000"  # ポート番号
+host="127.0.0.1"  # ローカルホストのIPアドレス
+role="none" # 役職：指定するときはすべて大文字
+times=1  # 実行回数
 
 while getopts ":p:h:r:t:" opt; do
-  case $opt in
-    p) port="$OPTARG"
-    ;;
-    h) host="$OPTARG"
-    ;;
-    r) role="$OPTARG"
-    ;;
-    t) times="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
+    case $opt in
+        p) port="$OPTARG"
+        ;;
+        h) host="$OPTARG"
+        ;;
+        r) role="$OPTARG"
+        ;;
+        t) times="$OPTARG"
+        ;;
+        \?) echo "Invalid option -$OPTARG" >&2
+        ;;
+    esac
 done
 
 role=`echo $role | tr "[:lower:]" "[:upper:]"`
 
 for i in $(seq 1 $times)
 do
-  latest_commit=$(git log -1 --pretty=format:"%H")
-  short_commit=${latest_commit:0:7}
-  date=`date "+%Y-%m-%d_%H.%M.%S"`
-  filename=log_client/${date}_${short_commit}.log
-  # -u でバッファリング無効
-  # tee で標準出力とファイル出力を同時に行う
-  python3 -u start.py -p $port -h $host -r $role 2>&1 | tee $filename
-  exit_status=$?
-  if (( exit_status != 0)); then
-    break
-  fi
-  if (( i != times )); then
-    sleep 5
-  fi
+    latest_commit=$(git log -1 --pretty=format:"%H")
+    short_commit=${latest_commit:0:7}
+    date=`date "+%Y-%m-%d_%H.%M.%S"`
+    filename=log_client/${date}_${short_commit}.log
+    # -u でバッファリング無効
+    # tee で標準出力とファイル出力を同時に行う
+    python3 -u start.py -p $port -h $host -r $role 2>&1 | tee $filename
+    exit_status=$?
+    if (( exit_status != 0)); then
+        break
+    fi
+    if (( i != times )); then
+        sleep 5
+    fi
 done
